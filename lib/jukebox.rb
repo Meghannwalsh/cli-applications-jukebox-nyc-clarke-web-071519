2e1b18e4a752035f67c3e8928bb4bac9fdba409a
Meghann McCool Walsh <meghann.mccool.walsh@gmail.com>
2019-07-13 16:03:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,7 +29,7 @@ def play(songs)
   song = gets.chomp
   if song.is_i? && song.to_i <= songs.length
     puts songs[song.to_i-1]
-  elsif song.is_a?(String)
+  else
     songs.each do |son|
     if son == song
       puts son
